9ca9b7a46ec84bbfcff2274ca50924cb5f5c7f0a
ernestmoi <ernestmoi@yahoo.com>
2020-02-09 11:26:42 +0100

    modified the api link

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function showError(error) {
 
 //GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${key}`;
 
     fetch(api)
         .then(function (response) {
